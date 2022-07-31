6593c4770b6a779d6d1e1816ea25c17c8716cde5
UdaySreddy <udayshankarkolamaram@gmail.com>
2022-07-31 12:54:32 +0000

    tostpen

--- src/components/Elements/index.js
+++ src/components/Elements/index.js
@@ -3,93 +3,27 @@ import './index.css'
 
 class Elements extends Component {
   render() {
+    const {resourseData} = this.props
+    const {category, description, iconUrl, link, title} = resourseData
+
     return (
-      <div className="ffgff">
+      <li className="cardsContainer1">
         <div className="container1">
           <div className="inner1">
             <div className="imageContainer">
-              <img
-                className="image1"
-                src="http://loremflickr.com/640/480"
-                alt="icon"
-              />
+              <img className="image1" src={iconUrl} alt="icon" />
             </div>
             <div>
-              <h1 className="Hedding1">Nickelson and Sons</h1>
-              <p className="Para1">Automotive</p>
+              <h1 className="Hedding1">{title}</h1>
+              <p className="Para1">{category}</p>
             </div>
           </div>
           <div className="inner2">
-            <p className="para2">https://gaseous-pod.net</p>
-            <p className="para3">
-              Eligendi cum eligendi nemo accusamus natus vero dolor.
-            </p>
+            <p className="para2">{link}</p>
+            <p className="para3">{description}</p>
           </div>
         </div>
-        <div className="container1">
-          <div className="inner1">
-            <div className="imageContainer">
-              <img
-                className="image1"
-                src="http://loremflickr.com/640/480"
-                alt="icon"
-              />
-            </div>
-            <div>
-              <h1 className="Hedding1">Nickelson and Sons</h1>
-              <p className="Para1">Automotive</p>
-            </div>
-          </div>
-          <div className="inner2">
-            <p className="para2">https://gaseous-pod.net</p>
-            <p className="para3">
-              Eligendi cum eligendi nemo accusamus natus vero dolor.
-            </p>
-          </div>
-        </div>
-        <div className="container1">
-          <div className="inner1">
-            <div className="imageContainer">
-              <img
-                className="image1"
-                src="http://loremflickr.com/640/480"
-                alt="icon"
-              />
-            </div>
-            <div>
-              <h1 className="Hedding1">Nickelson and Sons</h1>
-              <p className="Para1">Automotive</p>
-            </div>
-          </div>
-          <div className="inner2">
-            <p className="para2">https://gaseous-pod.net</p>
-            <p className="para3">
-              Eligendi cum eligendi nemo accusamus natus vero dolor.
-            </p>
-          </div>
-        </div>
-        <div className="container1">
-          <div className="inner1">
-            <div className="imageContainer">
-              <img
-                className="image1"
-                src="http://loremflickr.com/640/480"
-                alt="icon"
-              />
-            </div>
-            <div>
-              <h1 className="Hedding1">Nickelson and Sons</h1>
-              <p className="Para1">Automotive</p>
-            </div>
-          </div>
-          <div className="inner2">
-            <p className="para2">https://gaseous-pod.net</p>
-            <p className="para3">
-              Eligendi cum eligendi nemo accusamus natus vero dolor.
-            </p>
-          </div>
-        </div>
-      </div>
+      </li>
     )
   }
 }
--- src/components/Resources/index.js
+++ src/components/Resources/index.js
@@ -5,31 +5,100 @@ import Elements from '../Elements'
 import './index.css'
 
 class Resources extends Component {
+  state = {resourceList: [], initialList: []}
+
+  componentDidMount() {
+    this.getresources()
+  }
+
+  getresources = async () => {
+    const response = await fetch(
+      'https://media-content.ccbp.in/website/react-assignment/resources.json',
+    )
+    const data = await response.json()
+    const fetchedData = data.map(each => ({
+      category: each.category,
+      description: each.description,
+      iconUrl: each.icon_url,
+      id: each.id,
+      link: each.link,
+      tag: each.tag,
+      title: each.title,
+    }))
+    console.log(fetchedData)
+    this.setState({resourceList: fetchedData, initialList: fetchedData})
+  }
+  filter1 = () => {
+    const {initialList} = this.state
+    const requestList = initialList.filter(each => each.tag === 'request')
+    this.setState({resourceList: requestList})
+  }
+  filter2 = () => {
+    const {initialList} = this.state
+    const requestList = initialList.filter(each => each.tag === 'user')
+    this.setState({resourceList: requestList})
+  }
+
+  filter3 = () => {
+    const {initialList} = this.state
+    this.setState({resourceList: initialList})
+  }
+
+  filterList = event => {
+    const {resourceList, initialList} = this.state
+    const x = event.target.value.toUpperCase()
+    console.log(x)
+    if (x === '') {
+      this.setState({resourceList: initialList})
+    } else {
+      const filterdSearch = resourceList.filter(each =>
+        each.title.toUpperCase().includes(x),
+      )
+      this.setState({
+        resourceList: filterdSearch,
+      })
+    }
+  }
+
   render() {
+    const {resourceList} = this.state
+
     return (
       <div className="pageBackground">
         <div className="tabNames">
-          <button type="button" className="resourcesButton">
+          <button
+            onClick={this.filter3}
+            type="button"
+            className="resourcesButton"
+          >
             Resources
           </button>
-          <button type="button" className="otherButton">
+          <button onClick={this.filter1} type="button" className="otherButton">
             Requests
           </button>
-          <button type="button" className="otherButton">
+          <button onClick={this.filter2} type="button" className="otherButton">
             Users
           </button>
         </div>
 
         <div className="searchBox">
           <img
+            className="searchImage1"
             src="https://res.cloudinary.com/dyei9ewrb/image/upload/v1659172817/assignment/Icon_rlcstu.png"
             alt="search"
           />
-          <input className="searchInput" type="search" placeholder="Search" />
+          <input
+            onChange={this.filterList}
+            className="searchInput"
+            type="input"
+            placeholder="Search"
+          />
         </div>
-        <div className="elementsContainer">
-          <Elements />
-        </div>
+        <ul className="elementsContainer">
+          {resourceList.map(each => (
+            <Elements resourseData={each} key={each.id} />
+          ))}
+        </ul>
       </div>
     )
   }
